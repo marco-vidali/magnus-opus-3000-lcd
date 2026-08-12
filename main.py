04871385d74136755d84e6ac9e8751d0b2e61a6b
marco-vidali <marcovidali@outlook.com>
2026-08-12 20:31:10 +0200

    Simplify script

--- main.py
+++ main.py
@@ -1,31 +1,8 @@
-import subprocess
-from time import sleep
 from lcd_i2c import LCD_I2C
-
-
-def is_pipedal_running():
-    result = subprocess.run(
-        ["systemctl", "is-active", "pipedald"],
-        capture_output=True,
-        text=True,
-        check=False,
-    )
-
-    return result.stdout.strip() == "active"
-
 
 lcd = LCD_I2C(0x27, 16, 2)
 
 lcd.backlight.on()
 lcd.clear()
 
-lcd.write_text("Booting...")
-
-while True:
-    if is_pipedal_running():
-        break
-
-    sleep(1)
-
-lcd.clear()
 lcd.write_text("MAGNUS OPUS 3000")
